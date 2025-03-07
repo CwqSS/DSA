c25ebf0dd6ea1a0bf30055f5507b333da7be65f3
CwqSS <cauawillqssacramento@gmail.com>
2025-03-07 19:42:51 -0300

    feat(stack): adding list based stack without testing

---
+++ lifo/list-based/stack.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+sNode * createSNode(int value) {
+    sNode * n = malloc(sizeof(sNode));
+    n->value = value;
+    n->previous = NULL; 
+}
+stack * createStack() {
+    stack * S = malloc(sizeof(stack));
+    S->top = NULL;
+    return S;
+}
+int isStackEmpty(stack * S) {
+    if(S->top == NULL)
+        return 1;
+    else return 0;
+}
+void push(stack * S, int element) {
+    sNode * x = createSNode(element);
+    x->previous = S->top;
+    S->top = x;
+}
+int pop(stack * S) {
+    if(isStackEmpty(S)) {
+        return INT_MAX;
+    }
+    sNode * x = S->top;
+    S->top = x->previous;
+    int value = x->value;
+    free(x);
+    return value;
+}
+void freeStack(stack ** S) {
+    if(*S != NULL) {
+        if((*S)->top != NULL) {
+            sNode * x = (*S)->top;
+            sNode * y;
+            while(x != NULL) {
+                y = x;
+                x = x->previous;
+                free(y);
+            }
+        }
+        free(*S);
+        *S = NULL;
+    }
+}
---
+++ lifo/list-based/stack.h
@@ -0,0 +1,17 @@
+#ifndef STACK
+#define STACK
+typedef struct sNode {
+    int value;
+    struct sNode * previous;
+} sNode;
+typedef struct stack {
+    sNode * top;
+} stack;
+sNode * createSNode(int value);
+stack * createStack(int size);
+// int isStackFull(stack * S);
+int isStackEmpty(stack * S);
+void push(stack * S, int element);
+int pop(stack * S);
+void freeStack(stack ** S);
+#endif
